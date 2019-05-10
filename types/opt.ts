c5b7103bdfe1d2ca8404948f7cfa85950d7bff15
arvitaly <arvitaly@list.ru>
2019-05-10 11:39:23 +0300

    fixed opt type

--- types/opt.ts
+++ types/opt.ts
@@ -1,6 +1,6 @@
 import { typio } from "./../typio";
 
-export function opt<T>(obj: T): T | undefined | void {
+export function opt<T>(obj: T): T | undefined | never {
     return ((value: any) => (typeof value === "undefined" ? undefined : typio(value, obj as any))) as any;
 }
 export default opt;
